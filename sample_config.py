50d82aeab377e592e10a232d708bdd5228d9c521
updato111 <69454933+updato111@users.noreply.github.com>
2024-06-09 09:27:59 +0330

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,7 +7,7 @@ load_dotenv(
 )
 
 BOT_TOKEN = os.environ.get("6370891060:AAHjFCrStAuz7ng0bpAUJDrqxJhnL_e9a1k")
-API_ID = int(os.environ.get("24638763"))
+API_ID = int(os.environ.get(24638763))
 SESSION_STRING = os.environ.get("BABO-fIWQP2VSTFEHEBdMhJ9PufWtuo7FrLaqhhGglOmYpJ7HG4RL0wkrmXmqHXzRqh6PQcLIbpXWPliOhuCIpgPXYdlaFGvNV3e4HxAMcqd49grz2tjykvLnpxnrf44byHQ5qxjLYX0GsJb84PFnBvt2P-OmaBlW-hKvqf_A4twGG_ZrOdaTsbfvR92lbnevb5LnLcYL6M4fPVsCQQjBkxIo7R5LCJeGnfJO0kXFkiPkkqWFUC-wi0ElZ6C1b1uZCTaKG4ldiVBN9xilbIUYInxgDseE5t12_W_wJYDHZ1vYnhV72NheXn5oamUyh4_Aypl28lEyu3PD_oOkA_us5_AAAAAAVO5jmQA", "")
 API_HASH = os.environ.get("e02a72843ababb75de3c4f3aa2ce8d75")
 USERBOT_PREFIX = os.environ.get("USERBOT_PREFIX", ".")
